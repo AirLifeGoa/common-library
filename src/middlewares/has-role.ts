2dd4623523127e9fab1a5365eb345276970af20f
Divyansh <divyansh.24888@gmail.com>
2023-01-19 01:25:08 +0530

    Export has role and change some logic

--- src/middlewares/has-role.ts
+++ src/middlewares/has-role.ts
@@ -4,11 +4,13 @@ import {AuthorisationError} from "../errors/authorisation-error";
 // !! To be used once currentUser middleware is used
 // !! And require-auth middleware is used to verify that the user is logged in
 
-export const hasRole : any = (role: string) => {
-    return hasRole[role] || (hasRole[role] = (req: Request, res: Response, next: NextFunction) => {
-        if (!req.currentUser?.roles[role]) {
-            throw new AuthorisationError('Unauthorised action');
-        }
-        next();
-    });
+export const hasRole = (role:string) => {
+    return function(req:Request, res:Response, next:NextFunction) {
+       if(!req.currentUser?.roles[role]) {
+           throw new AuthorisationError("Authorisation Error you are not authorised");
+       }
+       next();
+    }
 }
+
+
