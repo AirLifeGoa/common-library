e512d95b5816c2631d2f6bbe56e5ad6edb621982
Divyansh <divyansh.24888@gmail.com>
2023-01-19 00:11:54 +0530

    Export has role and change some logic

--- src/middlewares/has-role.ts
+++ src/middlewares/has-role.ts
@@ -3,12 +3,37 @@ import {AuthorisationError} from "../errors/authorisation-error";
 
 // !! To be used once currentUser middleware is used
 // !! And require-auth middleware is used to verify that the user is logged in
-export const hasRole = (role: string) => {
-    return (req: Request, res: Response, next: NextFunction) => {
-        if (!req.currentUser?.roles[role]) {
-        throw new AuthorisationError('Not authorised');
+export const hasRole = {
+    admin: (req: Request, res: Response, next: NextFunction) => {
+        if (!req.currentUser?.roles.admin) {
+            throw new AuthorisationError('Not authorised');
         }
-
         next();
-    };
+    },
+
+    user: (req: Request, res: Response, next: NextFunction) => {
+        if (!req.currentUser?.roles.user) {
+            throw new AuthorisationError('Not authorised');
+        }
+        next();
+    },
+    manager: (req: Request, res: Response, next: NextFunction) => {
+        if (!req.currentUser?.roles.manager) {
+            throw new AuthorisationError('Not authorised');
+        }
+        next();
+    },
+    "dp-manager": (req: Request, res: Response, next: NextFunction) => {
+        if (!req.currentUser?.roles["dp-manager"]) {
+            throw new AuthorisationError('Not authorised');
+        }
+        next();
+    },
+    "data-analyst" : (req: Request, res: Response, next: NextFunction) => {
+        if (!req.currentUser?.roles["data-analyst"]) {
+            throw new AuthorisationError('Not authorised');
+        }
+        next();
+    },
+
 }
